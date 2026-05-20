0c355fbfbc9e49b8c0d06e4fc85c542b381f7af5
mampianina-andrianaivo <christianmampianina@gmail.com>
2026-05-20 16:41:40 +0300

    ok

--- blog.md
+++ blog.md
@@ -21,9 +21,11 @@ Ce film documentaire a été réalisé par Christophe LEROY et produit par le GR
 
 A Madagascar, si le système national de protection sociale reste embryonnaire, un lien de solidarité existe depuis si longtemps qu’il est devenu un mythe, le symbole d’une identité malgache, le Fihavanana Malagasy. Ce film rencontre les habitants d’un quartier populaire d’Antananarivo à la recherche de ce lien. Ce faisant, il croise sur son chemin une autre figure populaire, celle des deux malins, Ikotofestsy et Imaka, révélant l’association étroite entre le vivre ensemble et la contrainte sociale.
 
+<p style="text-align: left;">
 Pour en savoir plus :
 https://www.troisiemeporteagauche.com/trou-de-caimans/
 https://www.bse.u-bordeaux.fr/wp-content/uploads/2023/01/Presentation-film-Trou-de-Caiman_compressed.pdf
+</p>
 
 <br>
 
--- check.bat
+++ check.bat
@@ -1,25 +1,33 @@
 @echo off
 chcp 65001 > nul
-echo ===================================================
-echo   VÉRIFICATION DU DERNIER DÉPLOIEMENT GITHUB
-echo ===================================================
-echo.
 
-:: Vérifie si GitHub CLI est connecté
+:VERIF_AUTH
+:: Vérifie si GitHub CLI est bien connecté
 gh auth status >nul 2>&1
 if %errorlevel% neq 0 (
     color 0C
+    echo ===================================================
     echo [ERREUR] Vous devez d'abord vous connecter.
     echo Ouvrez un terminal et tapez : gh auth login
+    echo ===================================================
     echo.
     pause
     exit /b
 )
 
-:: Affiche le dernier déploiement
+:BOUCLE
+cls
+echo ===================================================
+echo   SUIVI EN DIRECT DU DÉPLOIEMENT GITHUB (Fermer pour quitter)
+echo ===================================================
+echo.
 echo Statut actuel :
+:: Récupère et affiche la toute dernière action
 gh run list --limit 1
 echo.
 echo ===================================================
-echo Appuyez sur une touche pour fermer cette fenêtre.
-pause > nul
+echo [Dernière mise à jour : %time:~0,8%] - Prochaine vérification dans 10s...
+
+:: Attend 10 secondes avant la prochaine actualisation
+timeout /t 10 > nul
+goto BOUCLE
